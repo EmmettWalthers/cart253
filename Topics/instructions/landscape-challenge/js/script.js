854dc2889b52eb720eeaa23f133b19511b871e20
EmmettWalthers <itsemmettdesign@gmail.com>
2025-09-12 13:42:36 -0400

    Finished drawing landscape

--- Topics/instructions/landscape-challenge/js/script.js
+++ Topics/instructions/landscape-challenge/js/script.js
@@ -1,21 +1,62 @@
 /**
- * Title of Project
- * Author Name
+ * Landscape Challenge
+ * Emmett Walthers
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * A beautiful art piece to showcase the use of draw functions!
  */
 
 "use strict";
 
 
 function setup() {
-    // Once at the beginning of the program
-    createCanvas(640, 480);
+    // Creates Canvas
+    createCanvas(640, 640);
 }
 
 function draw() {
     // Every Frame
-    background(255, 100, 100);
-    rect(200, 80, 240, 320);
+    background(135, 206, 235);
+
+    push();
+    fill('#007C00')
+    noStroke()
+    ellipse(110, 640, 800, 400);
+    pop();
+
+    push();
+    fill('#007C00')
+    noStroke()
+    ellipse(500, 640, 800, 400);
+    pop();
+
+    push();
+    fill('#854F28')
+    noStroke()
+    rect(100, 200, 50, 300);
+    pop();
+
+    push();
+    fill('#007C00')
+    noStroke()
+    ellipse(125, 200, 150);
+    pop();
+
+    push();
+    fill('#D1DFF6')
+    noStroke()
+    ellipse(450, 100, 150, 100);
+    pop();
+
+    push();
+    fill('#D1DFF6')
+    noStroke()
+    ellipse(400, 125, 150, 100);
+    pop();
+
+    push();
+    fill('#D1DFF6')
+    noStroke()
+    ellipse(500, 125, 150, 100);
+    pop();
+
 }
