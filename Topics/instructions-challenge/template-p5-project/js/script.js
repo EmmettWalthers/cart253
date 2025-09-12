c6322e90ea8a7e0a3be4249b7326beb2e3c97c7c
EmmettWalthers <itsemmettdesign@gmail.com>
2025-09-12 11:41:10 -0400

    Started the instructions challenge

---
+++ Topics/instructions-challenge/template-p5-project/js/script.js
@@ -0,0 +1,20 @@
+/**
+ * Title of Project
+ * Author Name
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+
+
+function setup() {
+    // Once at the beginning of the program
+    createCanvas(640, 480);
+}
+
+function draw() {
+    // Every Frame
+    background(255, 100, 100);
+}
