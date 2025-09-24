601e76758a1674def5c7adce74c43721a938fb4b
EmmettWalthers <itsemmettdesign@gmail.com>
2025-09-24 11:16:04 -0400

    Started working on face and background
    Created a sky blue background with a big head in the middle

--- Topics/art-jam/js/script.js
+++ Topics/art-jam/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * Emmett's Art Jam
+ * Emmett Walthers
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
@@ -8,17 +8,55 @@
 
 "use strict";
 
+
+
+const head = {
+    x: 300,
+    y: 300,
+    size: 500,
+    fill: "#F7D5BA"
+};
+
+const eyes = {
+    size: 125,
+    fill: "#FFFFFF"
+};
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates The Canvas
 */
 function setup() {
-
+    createCanvas(600, 600);
 }
+
+function drawFace() {
+    push();
+    noStroke();
+    fill(head.fill);
+    ellipse(head.x, head.y, head.size);
+    pop();
+
+    push();
+    noStroke();
+    fill(eyes.fill);
+    ellipse(200, 250, eyes.size);
+    pop();
+
+    push();
+    noStroke();
+    fill(eyes.fill);
+    ellipse(400, 250, eyes.size);
+    pop();
+}
+
+
 
 
 /**
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background("#00B9F7");
+    drawFace();
 
 }
