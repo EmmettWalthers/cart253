b055396caa610a5fd85ad35ae9850b4c636dde79
EmmettWalthers <itsemmettdesign@gmail.com>
2025-09-30 18:30:38 -0400

    Added comments!

--- Topics/art-jam/js/script.js
+++ Topics/art-jam/js/script.js
@@ -2,8 +2,8 @@
  * Emmett's Art Jam
  * Emmett Walthers
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Very acurate self portrait that reacts to pain! 
+ * 
  */
 
 "use strict";
@@ -12,10 +12,13 @@
  * Creates The Canvas
 */
 
+/** Pink variables changed eye color, pink1 is left eye, pink2 is right eye */
 let pink1 = 255;
 let pink2 = 255;
+/** Sound Effect */
 let ouch;
 
+/** Creates canvas and loads sound effect */
 function setup() {
     createCanvas(600, 600);
     ouch = loadSound('assets/sounds/classic_hurt.mp3');
@@ -29,13 +32,14 @@ function drawFace() {
     ellipse(300, 300, 400);
     pop();
 
-    /** Draws Eyes */
+    /** Draws Left Eye */
     push();
-    fill(255, pink1, pink1);
+    fill(255, pink1, pink1); /** '255' keeps eye red and 'pink' variable removes other colors */
     noStroke();
     arc(210, 240, 100, 100, 0, PI);
     pop();
 
+    /** Draws Right Eye */
     push();
     fill(255, pink2, pink2);
     noStroke();
@@ -61,6 +65,7 @@ function drawFace() {
 
 function mousePressed() {
 
+    /** If Mouse is within the semi-circle/eye and pink variable is under 50 it will remove 50 from variable and play sfx */
     if (dist(mouseX, mouseY, 210, 240) < 50 && mouseY > 240) {
         if (pink1 > 50) {
             pink1 -= 50
@@ -77,10 +82,12 @@ function mousePressed() {
 }
 
 function drawPupils() {
-    /** Draws Pupils */
+
+    /** Constrains pupils to the eye */
     let x1 = constrain(mouseX, 185, 235);
     let x2 = constrain(mouseX, 365, 415);
 
+    /** Draws Pupils */
     push();
     fill(0);
     noStroke();
@@ -89,13 +96,13 @@ function drawPupils() {
     pop();
 }
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+/** Runs the program - draws background, calls draw functions (ie: drawFace). */
 function draw() {
     background("#00B9F7");
     drawFace();
     drawPupils();
+    
+    /** If pink variable is under 255, continuously adds 1.5 to it until it reaches 255*/
     if (pink1 < 255) {
         pink1 += 1.5
     } 
